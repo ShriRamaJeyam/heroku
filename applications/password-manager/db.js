d909e5a654709148172f5bbd440d4ae47df45f58
Niranjan Kumar K S <niranks@amazon.com>
2021-03-20 19:34:24 +0530

    next

--- applications/password-manager/db.js
+++ applications/password-manager/db.js
@@ -5,73 +5,79 @@ const dbURI = (
     'postgres://postgres:niranjan@kshirsagar/heroku_local'
 );
 
-const sequelize = new Sequelize(dbURI);
+const sequelize = new Sequelize(dbURI, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
-class PasswordManager_Users extends Model {}
-class PasswordManager_Tags extends Model {}
-class PasswordManager_Passwords extends Model {}
+class PasswordManager_Users extends Model { }
+class PasswordManager_Tags extends Model { }
+class PasswordManager_Passwords extends Model { }
 
 PasswordManager_Users.init({
-    username : {
-        unique : true,
+    username: {
+        unique: true,
         type: DataTypes.STRING(15),
-        validate : {
-            is:{
-                args : /^[a-z]*$/g,
-                msg : "Username can contain only lowercase alphabets."
+        validate: {
+            is: {
+                args: /^[a-z]*$/g,
+                msg: "Username can contain only lowercase alphabets."
             },
             isMinLength(value) {
-                if(value.length < 8)
-                {
+                if (value.length < 8) {
                     throw "Username must be minimum of 8 characters";
                 }
             }
         }
     },
-    hash : {
-        type : DataTypes.STRING(64),
-        allowNull : false 
+    hash: {
+        type: DataTypes.STRING(64),
+        allowNull: false
     }
-},{sequelize});
+}, { sequelize });
 
 PasswordManager_Tags.init({
-    tag : {
-        unique : true,
+    tag: {
+        unique: true,
         type: DataTypes.STRING(15),
-        allowNull:false,
-        validate : {
-            is:{
-                args : /^[a-z]*$/g,
-                msg : "Tags can contain only lowercase alphabets."
+        allowNull: false,
+        validate: {
+            is: {
+                args: /^[a-z]*$/g,
+                msg: "Tags can contain only lowercase alphabets."
             }
         }
     }
-},{sequelize});
+}, { sequelize });
 
 PasswordManager_Passwords.init({
-    user : {
-        type : DataTypes.INTEGER,
-        references : {
-            model : PasswordManager_Users,
-            key : 'id'
+    user: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: PasswordManager_Users,
+            key: 'id'
         },
-        allowNull : false
+        allowNull: false
     },
-    password:{
-        type : DataTypes.ARRAY(DataTypes.STRING)
+    password: {
+        type: DataTypes.ARRAY(DataTypes.STRING)
     },
-    tags : {
-        type : DataTypes.ARRAY(DataTypes.INTEGER)
+    tags: {
+        type: DataTypes.ARRAY(DataTypes.INTEGER)
     }
-},{ sequelize });
+}, { sequelize });
 
 sequelize.sync({})
-.then(() => console.log("PostgreSQL DB authentication success"))
-.catch((error) => console.error(error));
+    .then(() => console.log("PostgreSQL DB authentication success"))
+    .catch((error) => console.error(error));
 
-module.exports = { 
-    sequelize, 
-    Users : PasswordManager_Users,
-    Tags:PasswordManager_Tags,
-    Passwords:PasswordManager_Passwords 
+module.exports = {
+    sequelize,
+    Users: PasswordManager_Users,
+    Tags: PasswordManager_Tags,
+    Passwords: PasswordManager_Passwords
 };
--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 module.exports = env = {
     pswd_mngr : {
-        admin_pass : 'NirU26@^',
+        admin_pass : 'SriRama108!)*',
         req_auth_timeout : 5000
     }
 };
